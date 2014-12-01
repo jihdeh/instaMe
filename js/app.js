f3b139d20f968b95ada5c08c92fa6aaa2e2d8dc0
Jihdeh <jide.b.tade@gmail.com>
2014-12-01 17:14:29 +0100

    Added Api key and instagram token

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 $(function(){
-// var url = 
+var url = "https://api.instagram.com/v1/media/popular?access_token=7ea79a955bc64d6abeacbcaed2ebedb3"
 
-
+$.ajax(url, {
+	dataType: "jsonp",
+	cache: "value", 
+	success: function(data){
+		for(var i=0; i< 6; i++){
+			$('#gallery').append('<ul><li><img src=' + data.data[i].images.low_resolution.url + '/>' + '</li></ul>');
+		}
+	}
+});//End ajax Call
 
 
 });//End Jquery
