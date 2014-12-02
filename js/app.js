ec1bc14862a6363742aca76e8227f2520042b721
Jihdeh <jide.b.tade@gmail.com>
2014-12-02 11:25:07 +0100

    Created Search for instagram api

--- js/app.js
+++ js/app.js
@@ -1,26 +1,17 @@
 $(function(){
-$('form').submit(function(evt){
-	evt.preventDefault();
-	var url = "https://api.instagram.com/v1/media/popular?client_id=4d90e5c04a4246bca4a7cccd613eb5bb";
-	var $search = $('#search');
-	var $submit = $('#submit');
-	var name = $search.val();
- var instaOptions = {
- 	tags: name,
- 	format: "jsonp"
- };
-$.ajax(url, instaOptions, {
+// var url = "https://api.instagram.com/v1/media/popular?access_token=7ea79a955bc64d6abeacbcaed2ebedb3";
+
+$.ajax({
 	type: "GET",
 	dataType: "jsonp",
 	cache: false, 
-	tags: name,
-	
-	success: function (response){
-		for (var i = 0; i < 6; i++) {
-          $("#insta_images").append("<li><img src='" + response.data[i].images.low_resolution.url + "'/></li>");
-    }
+	url: "https://api.instagram.com/v1/media/popular?client_id=7ea79a955bc64d6abeacbcaed2ebedb3",
+	success: function (data){
+		for(var i=0; i< 6; i++){
+			$('#gallery').append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></li>");
+		}
 	}
 });//End ajax Call
-});
+
 
 });//End Jquery
