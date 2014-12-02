b96681a4248e18b4677931e77c5e133db1499423
Jihdeh <jide.b.tade@gmail.com>
2014-12-02 09:23:16 +0100

    Created Search for instagram api

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 $(function(){
-var url = "https://api.instagram.com/v1/media/popular?access_token=7ea79a955bc64d6abeacbcaed2ebedb3"
-
-$.ajax(url, {
+$('form').submit(function(evt){
+	evt.preventDefault();
+	var url = "https://api.instagram.com/v1/media/popular?client_id=4d90e5c04a4246bca4a7cccd613eb5bb";
+	var $search = $('#search');
+	var $submit = $('#submit');
+	var name = $search.val();
+ var instaOptions = {
+ 	tags: name
+ };
+$.ajax(url, instaOptions, {
+	type: "GET",
 	dataType: "jsonp",
-	cache: "value", 
-	success: function(data){
-		for(var i=0; i< 6; i++){
-			$('#gallery').append('<ul><li><img src=' + data.data[i].images.low_resolution.url + '/>' + '</li></ul>');
-		}
+	cache: false, 
+	tags: name,
+	
+	success: function (response){
+		for (var i = 0; i < 6; i++) {
+          $("#insta_images").append("<li><img src='" + response.data[i].images.low_resolution.url + "'/></li>");
+    }
 	}
 });//End ajax Call
-
+});
 
 });//End Jquery
