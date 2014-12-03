d5253e1c3fef56476a9da83083ad6af879d64120
Jihdeh <jide.b.tade@gmail.com>
2014-12-03 10:55:04 +0100

    Search Input now fetches data from Instagram

--- js/app.js
+++ js/app.js
@@ -1,17 +1,40 @@
-$(function(){
-// var url = "https://api.instagram.com/v1/media/popular?access_token=7ea79a955bc64d6abeacbcaed2ebedb3";
 
-$.ajax({
-	type: "GET",
-	dataType: "jsonp",
-	cache: false, 
-	url: "https://api.instagram.com/v1/media/popular?client_id=7ea79a955bc64d6abeacbcaed2ebedb3",
-	success: function (data){
-		for(var i=0; i< 6; i++){
-			$('#gallery').append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></li>");
-		}
-	}
-});//End ajax Call
+var instagram = {
 
+  initialize: function(){
+    instagram.forms();
+  },
 
-});//End Jquery
+  display: function(photos){
+    $.each(photos.data, function(index, photos){
+      if(index === 6){
+        return false;
+      }
+      photos = "<li>" +
+           "<a href='" + photos.link + "' target='_blank'>" + 
+           "<img class='main' src='" + photos.images.low_resolution.url + "' width='250'  height='250' />" +
+                   "</a>" +
+                   "<img class='avatar' width='40' height='40' src='" + photos.user.profile_picture + "' />" +
+                   "<span class='heart'><strong>" + photos.likes.count + "</strong></span>" +
+                   "</li>";
+              $('#insta_images').append(photos);
+          
+    });
+  },
+
+  tags: function(tag){
+    var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=4d90e5c04a4246bca4a7cccd613eb5bb";
+    $.getJSON(url, instagram.display);
+  },
+
+  forms: function(){
+    $('form').submit(function (evt){
+      evt.preventDefault();
+      var $search = $('#search');
+      var $submit = $('#submit');
+      var tag = $('#search').val();
+      instagram.tags(tag);
+    });//End Submit
+  }
+};
+$(document).ready(instagram.initialize);
